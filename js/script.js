82aac314bd270ebe59730e33b8927b3d7bbc8b1d
mehedihasanme01 <mehedihasanme01@gmail.com>
2017-12-12 13:15:33 +0600

    final upload

---
+++ js/script.js
@@ -0,0 +1,94 @@
+
+
+
+
+$('#btn').click(function () {
+    var firstNumberValue = $('#firstNumber').val();
+    var lastNumberValue = $('#lastNumber').val();
+    var total = Number(firstNumberValue+lastNumberValue);
+    $('#total').val(total);
+
+});
+
+// $('#btn').click(function () {
+//     var firstNameValue = $('#firstName').val();
+//     var lastNameValue = $('#lastName').val();
+//     var fullName = firstNameValue+' '+lastNameValue;
+//     $('#fullName').val(fullName);
+//
+// });
+
+// alert($('#h1').text());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var redButtonElement = document.getElementById('btnRed');
+// redButtonElement.onclick = function () {
+//     var divElement = document.getElementById('demo');
+//      // divElement.style.backgroundColor = 'red';
+//      divElement.className ='class-two';
+// };
+//
+// var greenButtonElement = document.getElementById('btnGreen');
+// greenButtonElement.onclick = function () {
+//     var divElement = document.getElementById('demo');
+//     // divElement.style.backgroundColor = 'green';
+//     divElement.className ='class-one';
+// };
+//
+// var blueButtonElement = document.getElementById('btnBlue');
+// blueButtonElement.onclick = function () {
+//     var divElement = document.getElementById('demo');
+//     // divElement.style.backgroundColor = '#17a2b8';
+//     divElement.className ='class-three';
+// };
+//
+// var defaultButtonElement = document.getElementById('btnDefault');
+// defaultButtonElement.onclick = function () {
+//     var divElement = document.getElementById('demo');
+//     // divElement.style.backgroundColor = 'white';
+//     divElement.className ='mystyle';
+// };
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// var btnElement = document.getElementById('btn');
+// btnElement.onclick = function () {
+//  var startingNumberValue = document.getElementById('startingNumber').value;
+//  var endingNumberValue = document.getElementById('endingNumber').value;
+//   var res='';
+//  for(var x=startingNumberValue; x<=endingNumberValue; x++){
+//      res+=x;
+//  }
+//   document.getElementById('result').value =res;
+// };
+//
